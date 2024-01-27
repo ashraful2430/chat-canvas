9f39bd814b76b9a70e6576e99f711c5abb3c844b
ashraful2430 <www.savage582@gmail.com>
2024-01-27 21:25:06 +0600

    tested the add post section

--- cypress/e2e/addPoast.cy.js
+++ cypress/e2e/addPoast.cy.js
@@ -1,16 +1,13 @@
 describe("Add Post form", () => {
   it("should submit a post successfully", () => {
-    cy.visit("http://localhost:5173/dashboard/add-post"); // Replace with the actual route
+    cy.visit("http://localhost:5173/dashboard/add-post");
 
-    // Fill in the form fields
     cy.get('[data-cy="post-tag"]').select("TechTalk");
     cy.get('[data-cy="post-title"]').type("Test Post");
     cy.get('[data-cy="post-details"]').type("This is a test post");
 
-    // Submit the form
     cy.get(' [data-cy="add-post"]').click();
 
-    // Assert successful submission
     cy.visit("http://localhost:5173/dashboard/my-post");
   });
 });
