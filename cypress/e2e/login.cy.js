7936dca0734341c23bf1ea21234993e200938a93
ashraful2430 <www.savage582@gmail.com>
2024-01-27 19:49:19 +0600

    successfully tested the login features

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,18 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:5173");
-    cy.get("#tag-title").should("exist");
+describe("Login", () => {
+  it("displays errors for invalid inputs", () => {
+    cy.visit("http://localhost:5173/login");
+    cy.get('[data-cy="login-button"]').click();
+    cy.get('[data-cy="login-error-email"]').should("exist");
+    cy.get('[data-cy="login-pass-error"]').should("exist");
+  });
+
+  it("logs in with valid credentials", () => {
+    cy.visit("http://localhost:5173/login");
+    cy.get('[data-cy="login-email"]').type("cypres@test7.com");
+    cy.get('[data-cy="login-pass"]').type("123456Q!A%4d");
+    cy.get('[data-cy="login-button"]').click();
+
+    // Confirm successful login
+    cy.url().should("include", "http://localhost:5173/");
   });
 });
--- src/Pages/UserOrAdminLogIn/Login/Login.jsx
+++ src/Pages/UserOrAdminLogIn/Login/Login.jsx
@@ -66,7 +66,7 @@ const Login = () => {
               <span className="w-1/5 border-b  lg:w-1/4"></span>
             </div>
 
-            <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
+            <form data-cy="login-form" onSubmit={handleSubmit(onSubmit)}>
               <div className="mt-4">
                 <label
                   className="block mb-2 text-sm font-medium text-gray-600 "
@@ -75,7 +75,7 @@ const Login = () => {
                   Email Address
                 </label>
                 <input
-                  id="login-email"
+                  data-cy="login-email"
                   className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
                   type="email"
                   name="email"
@@ -83,7 +83,7 @@ const Login = () => {
                   placeholder="Your email"
                 />
                 {errors.email && (
-                  <span id="login-error-email" className="text-red-500">
+                  <span data-cy="login-error-email" className="text-red-500">
                     Your email is required
                   </span>
                 )}
@@ -103,7 +103,7 @@ const Login = () => {
                 </div>
 
                 <input
-                  id="login-pass"
+                  data-cy="login-pass"
                   className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg   focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
                   type={showPass ? "text" : "password"}
                   name="password"
@@ -115,7 +115,7 @@ const Login = () => {
                   placeholder="********"
                 />
                 {errors.password && (
-                  <span id="login-pass-error" className="text-red-500">
+                  <span data-cy="login-pass-error" className="text-red-500">
                     Your password is incorrect
                   </span>
                 )}
@@ -129,7 +129,7 @@ const Login = () => {
 
               <div className="mt-6">
                 <button
-                  id="login-button"
+                  data-cy="login-button"
                   type="submit"
                   className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
                 >
